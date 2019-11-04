bb73c73ac6e9f7f0d6ec9efdeb92e50b6ed5f919
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-11-03 20:14:05 -0500

    fetch users as a separate api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
@@ -7,3 +8,15 @@ export const fetchPosts = () => {
         dispatch({ type: 'FETCH_POSTS', payload: response.data})
     };
 };
+
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+});
+
+//you cant refetch a user using this action creator, you would have to re-fetch without memoization
