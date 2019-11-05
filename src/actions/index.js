32c2cd07f074e5baa71d36b2ee74b95af7b50bfa
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-11-04 20:55:00 -0500

    refactored call and reduced number of requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) =>{
+    await dispatch(fetchPosts());
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
+
 export const fetchPosts = () => {
     return async (dispatch) => {
         const response = await jsonPlaceholder.get('/posts');    
@@ -9,6 +19,14 @@ export const fetchPosts = () => {
     };
 };
 
+export const fetchUser = (id) => async dispatch => {
+
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+/*
 export const fetchUser = (id) => dispatch => {
     _fetchUser(id, dispatch);
 };
@@ -18,5 +36,6 @@ const _fetchUser = _.memoize(async (id, dispatch) => {
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
 });
+*/
 
 //you cant refetch a user using this action creator, you would have to re-fetch without memoization
